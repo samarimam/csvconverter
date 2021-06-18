fff9f9d033a6f4d289eca44f1e1cda9c9ca11bd2
samarimam <samarimam78@gmail.com>
2021-06-18 20:32:19 +0530

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Dashboard from './dashboard.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
 import Login from "./Login";
 
 var isLoggedIn = true;
